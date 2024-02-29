e4ba3ebe09f1c182fa795e8b7b6020aa75379d34
torressg <guilhermevanderlei0210@gmail.com>
2024-02-29 08:50:51 -0300

    create patch route

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,11 +1,11 @@
-import { Body, Controller, Get, Param, Post, Res } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, Res } from '@nestjs/common';
 import { ValidateNumericIdPipe } from 'src/validate-id/validate-id.interceptor';
 
 @Controller('courses')
 export class CoursesController {
   @Get()
   findAll(@Res() response) {
-    return response.status(200).json({message: 'Listagem de cursos'})
+    return response.status(200).json({ message: 'Listagem de cursos' })
   }
 
   @Get(':id')
@@ -16,5 +16,10 @@ export class CoursesController {
   @Post()
   create(@Body() body) {
     return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id', new ValidateNumericIdPipe()) id: number, @Body() body): string {
+    return `Curso ${id} atualizado.`
   }
 }
