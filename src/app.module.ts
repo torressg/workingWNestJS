0957f24ebf514d5f3158f2ded4e19f70bab4ce81
torressg <guilhermevanderlei0210@gmail.com>
2024-03-07 09:00:22 -0300

    create module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoursesController } from './courses/courses.controller';
 import { CoursesService } from './courses/courses.service';
+import { CoursesModule } from './courses/courses.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, CoursesController],
-  providers: [AppService, CoursesService],
+  imports: [CoursesModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpException, HttpStatus, Param, Post, Put } from '@nestjs/common';
 import { ValidateNumericIdPipe } from 'src/validate-id/validate-id.interceptor';
 import { CoursesService } from './courses.service';
 
@@ -13,7 +13,10 @@ export class CoursesController {
 
   @Get(':id')
   findOne(@Param('id', new ValidateNumericIdPipe()) id: number) {
-    return this.courseService.findOne(id)
+    const course = this.courseService.findOne(id)
+    if(!course) {
+      throw new HttpException(`Course ${id} not found.`, HttpStatus.NOT_FOUND)
+    }
   }
 
   @Post()
